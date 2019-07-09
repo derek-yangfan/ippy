228e6b5e95fd964b5db27657be9551b0e98b9281
derek-yangfan <yangfan06@126.com>
2019-07-09 13:31:14 +0800

    8

--- ip2int.py
+++ ip2int.py
@@ -8,7 +8,7 @@ def ip_to_long(ip_address):
     packed_ip = socket.inet_aton(ip_address)
     # 使用0是因为解包的是一个单元素的元组
     return struct.unpack('!L', packed_ip)[0]
-#sss33ss22222222222
+#sss33ss2222222222233333
 
 ip1 = ip_to_long('10.243.16.1')
 print (ip1)
