04a70919843378488f959c8125b945d72fa55e04
derek-yangfan <yangfan06@126.com>
2019-07-10 16:59:31 +0800

    1

--- assign_ip.py
+++ assign_ip.py
@@ -9,21 +9,22 @@ import functions as func
 if __name__ == "__main__":
     """系统自动分配输入网段的IP地址"""
 
+    """input IP address"""
+    i_site = (input(u'请输入Plant (例如Plant 51)： '))
+    i_line= (input(u'请输入Line： '))
+    i_cell = (input(u'请输入Cell： '))
+    count = int(input(u'请输入你需要IP的数量： '))
+
+
     """Initial connection to DB"""
     settings1 = settings.settings()
     db = settings1.db_connect()
     cursor = db.cursor()
 
-    """input IP address"""
-    IPbegin = (input(u'请输入起始查询IP： '))
-    ip_obj = ip.ip(IPbegin)
-    addr1 = ip_obj.address
-    subnet_int = ip_obj.get_subnet()
+    subnet = func.assign_subnets(cursor, i_site, i_line, i_cell)
+    subnet_int = func.ip_to_long(subnet)
 
-
-    count = int(input(u'请输入你需要IP的数量： '))
-
-    for s1 in range(subnet_int+1, subnet_int+100):
+    for s1 in range(subnet_int+1, subnet_int+255):
         if count >=1:
             if not func.query_ip(s1, cursor):
                 print(func.long_to_ip(s1) + ' is available to use')
--- functions.py
+++ functions.py
@@ -47,4 +47,22 @@ def query_ip(ip_address, cursor):
             #print('yes')
             return True
   
+def assign_subnets(cursor, site, line, cell):
+    "检查IP是否已经存在"
+       
+    sql = """select red, bm, descr, locations.loc as site, categoria, comentario, Line, Cell, client_id from 
+            (select red, bm, descr, loc, categoria, comentario, max(if(cc_id = '2', entry, 0)) 'Line', max(if(cc_id = '3', entry, 0)) 'Cell'
+            from custom_net_column_entries as a 
+            left join net on a.net_id = net.red_num 
+            group by net_id) as b 
+            left join locations on b.loc = locations.id where locations.loc ='%s' and line = '%s' and find_in_set('%s', cell);"""%(site, line, cell)
 
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for r1 in result:
+        if result == None:
+            print('no')
+        
+        else:
+            return r1[0]
+        
